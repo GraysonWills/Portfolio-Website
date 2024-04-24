84009a53b0b0ba6da07d113e17103d61144ef603
Grayson Wills <grayson.wills@gm.com>
2024-04-24 09:14:20 -0400

    Begin creationg of Projects page
    Started new branch yesterday (4/23/24) for the projects page branch.
    Planning on getting some of this running before and the look of it nice
    before moving on to other sections. This is also a good way to version
    out the code, branch the project, and make it a bit more organized in
    the testing and development environment. More to come on this

---
+++ Portfolio_Website/javascript/projects.js
@@ -0,0 +1,5 @@
+document.querySelector('.image-container').addEventListener('click', function() {
+    var lists = document.querySelector('.lists-container');
+    lists.style.display = lists.style.display === 'block' ? 'none' : 'block';
+  });
+  
